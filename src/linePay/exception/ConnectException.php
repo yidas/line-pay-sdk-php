c18d4e06d610ff5dc8736b8dd34a042c600348dd
Nick Tsai <myintaer@gmail.com>
2020-11-13 19:00:38 +0800

    Fix typo

--- src/linePay/exception/ConnectException.php
+++ src/linePay/exception/ConnectException.php
@@ -2,14 +2,14 @@
 
 namespace yidas\linePay\exception;
 
-use GuzzleHttp\Exception\ConnectException as BassException;
+use GuzzleHttp\Exception\ConnectException as BaseException;
 
 /**
  * Exception thrown when a connection cannot be established.
  *
  * Note that no response is present for a ConnectException
  */
-class ConnectException extends BassException
+class ConnectException extends BaseException
 {
     
 }
